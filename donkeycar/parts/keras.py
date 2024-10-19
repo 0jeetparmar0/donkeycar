4ccf739600b2ab70b9bc1f94a24010982346c35a
JEET PARMAR <88609937+0jeetparmar0@users.noreply.github.com>
2024-10-19 16:15:29 +0530

    Update keras.py
    add nvidia model

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -810,15 +810,15 @@ def core_cnn_layers(img_in, drop, l4_stride=1):
     :return:                stack of CNN layers
     """
     x = img_in
-    x = conv2d(24, 5, 2, 1)(x)
+    x = conv2d(24, 5, 2, 1, activation='elu' )(x)
     x = Dropout(drop)(x)
-    x = conv2d(32, 5, 2, 2)(x)
+    x = conv2d(36, 5, 2, 2, activation='elu')(x)
     x = Dropout(drop)(x)
-    x = conv2d(64, 5, 2, 3)(x)
+    x = conv2d(48, 5, 2, 3,activation='elu')(x)
     x = Dropout(drop)(x)
-    x = conv2d(64, 3, l4_stride, 4)(x)
+    x = conv2d(64, 3,  l4_stride,4,activation='elu')(x)
     x = Dropout(drop)(x)
-    x = conv2d(64, 3, 1, 5)(x)
+    x = conv2d(64, 3, 1, 5,activation='elu')(x)
     x = Dropout(drop)(x)
     x = Flatten(name='flattened')(x)
     return x
@@ -828,11 +828,14 @@ def default_n_linear(num_outputs, input_shape=(120, 160, 3)):
     drop = 0.2
     img_in = Input(shape=input_shape, name='img_in')
     x = core_cnn_layers(img_in, drop)
-    x = Dense(100, activation='relu', name='dense_1')(x)
+    x = Dense(1164, activation='elu', name='dense_1')(x)
     x = Dropout(drop)(x)
-    x = Dense(50, activation='relu', name='dense_2')(x)
+    x = Dense(100, activation='elu', name='dense_2')(x)
     x = Dropout(drop)(x)
-
+    x = Dense(50, activation='elu', name='dense_3')(x)
+    x = Dropout(drop)(x)
+    x = Dense(10, activation='elu', name='dense_4')(x)
+    x = Dropout(drop)(x)
     outputs = []
     for i in range(num_outputs):
         outputs.append(
